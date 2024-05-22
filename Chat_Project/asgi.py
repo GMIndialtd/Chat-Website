6a2eabe31a23f7827f564650a0efe6bbf2882c46
= <=>
2024-05-22 16:18:21 +0530

    I have added some configuration codes in consumers.py, routing.py and asgi.py for chat website also i have added some codes for my user chat-initiator functionalities in chatextras.py

--- Chat_Project/asgi.py
+++ Chat_Project/asgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import Chat_app.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Chat_Project.settings")
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            Chat_app.routing.websocket_urlpatterns
+        )
+    ),
+})
